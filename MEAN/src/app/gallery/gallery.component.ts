d76473f9e2f3a8d9ad3501ac4c6b670f9e166360
brian <brian22e3@hotmail.com>
2018-11-28 21:20:59 -0800

    add photoswipe

--- MEAN/src/app/gallery/gallery.component.ts
+++ MEAN/src/app/gallery/gallery.component.ts
@@ -1,15 +1,78 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var PhotoSwipe: any;
+declare var PhotoSwipeUI_Default: any;
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css', './photoswipe.css','./default-skin/default-skin.css']
 })
 export class GalleryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  loadAPI: Promise<any>;
+  constructor() {
+    this.loadAPI = new Promise(resolve => {
+      this.loadScript();
+      resolve(true);
+    });
   }
 
+  ngOnInit() {
+    const pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    const items = [
+      {
+        src: 'https://placekitten.com/600/400',
+        w: 600,
+        h: 400
+      },
+      {
+        src: 'https://placekitten.com/1200/900',
+        w: 1200,
+        h: 900
+      }
+    ];
+    // define options (if needed)
+    const options = {
+      // optionName: 'option value'
+      // for example:
+      index: 0 // start at first slide
+    };
+    // Initializes and opens PhotoSwipe
+    const gallery = new PhotoSwipe(
+      pswpElement,
+      PhotoSwipeUI_Default,
+      items,
+      options
+    );
+    gallery.init();
+  }
+
+  public loadScript() {
+    let isFound = false;
+    const scripts = document.getElementsByTagName('script');
+    for (let i = 0; i < scripts.length; ++i) {
+      if (
+        scripts[i].getAttribute('src') != null &&
+        scripts[i].getAttribute('src').includes('loader')
+      ) {
+        isFound = true;
+      }
+    }
+
+    if (!isFound) {
+      const dynamicScripts = [
+        'assets/js/photoswipe-ui-default.min.js',
+        'assets/js/photoswipe.min.js'
+      ];
+
+      for (let i = 0; i < dynamicScripts.length; i++) {
+        const node = document.createElement('script');
+        node.src = dynamicScripts[i];
+        node.type = 'text/javascript';
+        node.async = false;
+        node.charset = 'utf-8';
+        document.getElementsByTagName('head')[0].appendChild(node);
+      }
+    }
+  }
 }
